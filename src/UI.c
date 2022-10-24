f915249243443748b6faf2ece622bf043b3d3705
HYIND <1255231699@qq.com>
2022-10-24 09:12:30 -0700

    Init

--- src/UI.c
+++ src/UI.c
@@ -1,7 +1,7 @@
 #include "UI.h"
 #include "fd.h"
 
-void show_UI()
+void show_mainUI()
 {
     show_1152000bmp("windows_pic/main.bmp", p_lcd);
 }
--- src/main.c
+++ src/main.c
@@ -6,30 +6,32 @@ int main()
 {
     my_Init();
 
-    show_UI();
+    show_mainUI();
     while (1)
     {
         Get_Touch_Data();
         if (touch.type == EV_KEY && touch.code == BTN_TOUCH && touch.value == 0) //判断手是否离开
         {
             printf("x:%d--y:%d\n", P_I.x, P_I.y);
-            if (P_I.x > 20 && P_I.x < 220 && P_I.y > 300 && P_I.y < 440) //视频按钮
+            if (P_I.x > 20 && P_I.x < 220 && P_I.y > 300 && P_I.y < 440) //相册按钮
             {
                 // loadig();
                 // Video();
+                // show_mainUI();
             }
 
-            if (P_I.x > 310 && P_I.x < 480 && P_I.y > 300 && P_I.y < 440) //音乐按钮
+            if (P_I.x > 310 && P_I.x < 480 && P_I.y > 300 && P_I.y < 440) //多媒体按钮
             {
                 // loadig();
                 // Music();
+                // show_mainUI();
             }
 
             if (P_I.x > 580 && P_I.x < 770 && P_I.y > 300 && P_I.y < 440) //游戏按钮
             {
                 // loadig();
                 Game();
-                show_UI();
+                show_mainUI();
             }
 
             if (P_I.x > 340 && P_I.x < 440 && P_I.y > 180 && P_I.y < 280) //退出按钮
