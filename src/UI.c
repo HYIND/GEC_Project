0dd8f7c48e923024524d90d44552aff2a45a4d1f
Jaky347 <332321363@qq.com>
2022-10-27 00:20:32 -0700

    ui

--- src/UI.c
+++ src/UI.c
@@ -20,7 +20,7 @@ void show_albumUI()
     show_location_bmp("windows_pic/left.bmp", 0, 205, 40, 70, p_lcd);
     show_location_bmp("windows_pic/slide.bmp", 330, 420, 52, 52, p_lcd);
     show_location_bmp("windows_pic/delete.bmp", 418, 420, 52, 52, p_lcd);
-    show_location_bmp("windows_pic/exitphoto.bmp", 700, 0, 52 , 52, p_lcd);
+    show_location_bmp("windows_pic/exitphoto.bmp", 748, 0, 52 , 52, p_lcd);
 }
 
 void show_cameraUI()
@@ -38,14 +38,15 @@ void show_musicUI()
     show_1152000bmp("windows_pic/mainbkg.bmp", p_lcd);                     // 音乐界面背景
     show_location_bmp("windows_pic/musicUI.bmp", 0, 380, 500, 100, p_lcd); // 音乐界面控制台
     show_location_bmp("windows_pic/bigmusic.bmp", 125, 50, 252, 252, p_lcd);
-    show_location_bmp("windows_pic/exitmusic.bmp", 700, 0, 52, 52, p_lcd);
+    show_location_bmp("windows_pic/exitmusic.bmp", 748, 0, 52, 52, p_lcd);
 
 }
 
 void show_videoUI()
 {
     clear_lcd_screen(COLOR_WHITE, 0, 0, 800, 480, p_lcd);
-    show_location_bmp("windows_pic/musicUI.bmp", 150, 380, 500, 100, p_lcd); // 视频界面控制台
+    show_location_bmp("windows_pic/musicUI.bmp", 150, 380, 500, 100, p_lcd);
+    show_location_bmp("windows_pic/exitmp4.bmp", 748, 0, 52, 52, p_lcd); // 视频界面控制台
 
 }
 
