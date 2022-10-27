f3e7353c7892ed72d231c204fb4e60d8c540aaf0
Jaky347 <332321363@qq.com>
2022-10-27 00:51:09 -0700

    ui

--- src/UI.c
+++ src/UI.c
@@ -5,12 +5,7 @@
 
 void show_mainUI()
 {
-    show_1152000bmp("windows_pic/mainbkg.bmp", p_lcd);
-    show_location_bmp("windows_pic/photo.bmp", 50, 90, 100, 100, p_lcd);
-    show_location_bmp("windows_pic/mp3.bmp", 250, 90, 100, 100, p_lcd);
-    show_location_bmp("windows_pic/mp4.bmp", 450, 90, 100, 100, p_lcd);
-    show_location_bmp("windows_pic/game.bmp", 650, 90, 100, 100, p_lcd);
-    show_location_bmp("windows_pic/camera.bmp", 50, 290, 100, 100, p_lcd);
+    show_1152000bmp("windows_pic/main.bmp", p_lcd);
 }
 
 void show_albumUI()
